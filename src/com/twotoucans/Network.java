92f194714d80d3a84cb7c47054df9486c9da5326
ColtKing <king.colt@yahoo.ca>
2017-03-27 17:05:05 -0700

    LAPACK is stupid

--- src/com/twotoucans/Network.java
+++ src/com/twotoucans/Network.java
@@ -4,43 +4,68 @@ import java.util.Random;
 
 public class Network
 {
-	private int numLayers;
-	private int[] sizes;
-	private double[][] biases;
-	private double[][][] weights;
-	
-	public Network(int[] sizes)
-	{
-		numLayers = sizes.length;
-		this.sizes = sizes;
-		this.biases = new double[numLayers - 1][];
-		this.weights = new double[numLayers - 1][][];
+    private int numLayers;
+    private int[] sizes;
+    private double[][] biases;
+    private double[][][] weights;
+    
+    public Network(int[] sizes)
+    {
+        numLayers = sizes.length;
+        this.sizes = sizes;
+        this.biases = new double[numLayers - 1][];
+        this.weights = new double[numLayers - 1][][];
 
-		Random r = new Random();
-		
-		for (int i = 0; i < numLayers - 1; i++)
-		{
-			int size = this.sizes[i + 1];
-			biases[i] = new double[size];
-			for (int y = 1; y < size; y++)
-			{
-				biases[i][y] = r.nextGaussian();
-			}
-		}
-		
-		for (int i = 0; i < numLayers - 1; i++)
-		{
-			int x = this.sizes[i];
-			int y = this.sizes[i + 1];
-			weights[i] = new double[y][x];
-			
-			for (int j = 0; j < y; j++)
-			{
-				for (int n = 0; n < x; n++)
-				{
-					weights[i][y][x] = r.nextGaussian();
-				}
-			}
-		}
-	}
+        Random r = new Random();
+        
+        for (int i = 0; i < numLayers - 1; i++)
+        {
+            int size = this.sizes[i + 1];
+            biases[i] = new double[size];
+            for (int y = 1; y < size; y++)
+            {
+                biases[i][y] = r.nextGaussian();
+            }
+        }
+        
+        for (int i = 0; i < numLayers - 1; i++)
+        {
+            int x = this.sizes[i];
+            int y = this.sizes[i + 1];
+            weights[i] = new double[y][x];
+            
+            for (int j = 0; j < y; j++)
+            {
+                for (int n = 0; n < x; n++)
+                {
+                    weights[i][y][x] = r.nextGaussian();
+                }
+            }
+        }
+    }
+    
+    //Mini batch will be an array of input/outputs [FIX LATER]
+    public void update_mini_batch(int[] mini_batch, int eta)
+    {
+        /*Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The "mini_batch" is a list of tuples "(x, y)", and "eta"
+        is the learning rate.*/
+        
+        /*nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw 
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb 
+                       for b, nb in zip(self.biases, nabla_b)]*/
+        
+        
+        double[][] nabla_b = new double[biases.length][biases[0].length];
+        double[][] nabla_w = new double[weights.length][weights[0].length];
+        
+    }
 }
