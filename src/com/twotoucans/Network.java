49c4f16386810713a2e5535de780a5f08ed020ce
Aaron Freytag <aaroncfreytag@gmail.com>
2017-04-03 09:23:43 -0700

    Changed data values to use colt

--- src/com/twotoucans/Network.java
+++ src/com/twotoucans/Network.java
@@ -2,45 +2,38 @@ package com.twotoucans;
 
 import java.util.Random;
 
+import cern.jet.math.*;
+import cern.jet.random.*;
+import cern.jet.random.engine.RandomEngine;
+import cern.colt.matrix.*;
+import cern.colt.function.*;
+
 public class Network
 {
     private int numLayers;
     private int[] sizes;
-    private double[][] biases;
-    private double[][][] weights;
+    private DoubleMatrix1D[] biases;
+    private DoubleMatrix2D[] weights;
     
     public Network(int[] sizes)
     {
         numLayers = sizes.length;
         this.sizes = sizes;
-        this.biases = new double[numLayers - 1][];
-        this.weights = new double[numLayers - 1][][];
-
-        Random r = new Random();
+        this.biases = new DoubleMatrix1D[numLayers - 1];
+        this.weights = new DoubleMatrix2D[numLayers - 1];
+        
+        Normal gaussGen = new Normal(0.0, 1.0, RandomEngine.makeDefault());
         
         for (int i = 0; i < numLayers - 1; i++)
         {
-            int size = this.sizes[i + 1];
-            biases[i] = new double[size];
-            for (int y = 1; y < size; y++)
-            {
-                biases[i][y] = r.nextGaussian();
-            }
-        }
-        
-        for (int i = 0; i < numLayers - 1; i++)
-        {
-            int x = this.sizes[i];
-            int y = this.sizes[i + 1];
-            weights[i] = new double[y][x];
-            
-            for (int j = 0; j < y; j++)
-            {
-                for (int n = 0; n < x; n++)
-                {
-                    weights[i][y][x] = r.nextGaussian();
-                }
-            }
+        	biases[i] = DoubleFactory1D.dense.make(this.sizes[i + 1]);
+        	weights[i] = DoubleFactory2D.dense.make(this.sizes[i + 1], this.sizes[i]);
+        	
+        	biases[i].assign(gaussGen);
+        	weights[i].assign(gaussGen);
+        	
+        	System.out.println(biases[i]);
+        	System.out.println(weights[i]);
         }
     }
     
@@ -61,11 +54,11 @@ public class Network
         self.weights = [w-(eta/len(mini_batch))*nw 
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb 
-                       for b, nb in zip(self.biases, nabla_b)]*/
+                       for b, nb in zip(self.biases, nabla_b)]
         
         
         double[][] nabla_b = new double[biases.length][biases[0].length];
-        double[][] nabla_w = new double[weights.length][weights[0].length];
-        
+        double[][] nabla_w = new double[weights.length][weights[0].length];   
+        */
     }
 }
