b52cd46473bbf01eed5a2b08787976173f88b614
ColtKing <king.colt@yahoo.ca>
2017-04-08 18:48:45 -0700

    The main class is set up, just need Aaron to fix the TestData class

--- src/com/twotoucans/Main.java
+++ src/com/twotoucans/Main.java
@@ -1,15 +1,17 @@
 package com.twotoucans;
 
-import cern.colt.matrix.DoubleFactory1D;
-import cern.colt.matrix.DoubleMatrix1D;
-
 public class Main
 {
 	public static void main(String[] args)
 	{
 		int[] layers = {728, 30, 10};
 		Network n = new Network(layers);
-
-    	n.SGD(training_data, epochs, mini_batch_size, eta, test_data);
+		TestData training = new TestData(layers[layers.length-1]);
+		training.readFromFiles("./data/train-labels.idx1-ubyte", "./data/train-images.idx3-ubyte");
+		
+		TestData testing = new TestData(layers[layers.length-1]);
+        testing.readFromFiles("./data/t10k-labels.idx1-ubyte", "./data/t10k-images.idx3-ubyte");
+        
+    	n.SGD(training.getData(), 30, 10, 3, testing.getData());
 	}
 }
